991b04b22d6845bd48acb27d21b2c374b9258056
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2023-09-27 15:52:43 +0300

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,41 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import javax.validation.ValidationException;
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private int id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+
+    public User(int id, String email, String login, String name, LocalDate birthday) {
+        if (email == null || !email.contains("@")) {
+            throw new ValidationException("Некорректный формат электронной почты");
+        }
+
+        if (login == null || login.isEmpty() || login.contains(" ")) {
+            throw new ValidationException("Логин не может быть пустым и содержать пробелы");
+        }
+
+        if (name == null || name.isEmpty()) {
+            this.name = login;
+        } else {
+            this.name = name;
+        }
+
+        LocalDate currentDate = LocalDate.now();
+        if (birthday != null && birthday.isAfter(currentDate)) {
+            throw new ValidationException("Дата рождения не может быть в будущем");
+        }
+
+        this.id = id;
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
+}
