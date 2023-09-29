4e914c6e40d223e4543def10d7b5e2e2e9ce1b1a
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2023-09-29 23:32:09 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,45 +3,38 @@ package ru.yandex.practicum.filmorate.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.Collection;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private static final Logger log = LoggerFactory.getLogger(UserController.class);
-    private final List<User> users = new ArrayList<>(); // Используем ArrayList вместо массива
-    private Integer id = 0;
+    private static final Logger log = LoggerFactory.getLogger(FilmController.class);
+    private final HashMap<Integer, User> users = new HashMap<>();
+    Integer id = 0;
 
     @PostMapping
     public User createUser(@RequestBody User user) {
         user.setId(id);
-        users.add(user); // Добавляем пользователя в список
+        users.put(id, user);
         id++;
         log.debug("User создан");
         return user;
     }
 
-    @PutMapping("/{id}") // Используем путь для указания ID пользователя
-    public User updateUser(@PathVariable("id") Integer userId, @RequestBody User updatedUser) throws ValidationException {
-        if (userId >= 0 && userId < users.size()) {
-            updatedUser.setId(userId); // Устанавливаем ID из пути
-            users.set(userId, updatedUser); // Обновляем пользователя в списке
-            log.debug("User заменён");
-            return updatedUser;
-        } else {
-            throw new ValidationException("Некорректный ID пользователя");
-        }
+
+    @PutMapping
+    public User updateUser(@RequestBody User updatedUser) {
+        users.replace(updatedUser.getId(), updatedUser);
+        log.debug("User заменён");
+        return updatedUser;
     }
 
     @GetMapping
-    public List<User> getAllUsers() {
+    public Collection<User> getAllUsers(@RequestBody User user) {
         log.debug("Текущее количество пользователей: {}", users.size());
-        return users;
+        return users.values();
     }
 }
