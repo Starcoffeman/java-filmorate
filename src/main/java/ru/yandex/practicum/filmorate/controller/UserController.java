8a682f60263ed6403ab52ac2f156fd0e396d0dd9
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2023-09-30 11:41:11 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
+
 import java.util.Collection;
 import java.util.HashMap;
 
@@ -27,7 +28,7 @@ public class UserController {
 
     @PutMapping
     public User updateUser(@RequestBody User updatedUser) throws UserNotFoundException {
-        if(users.get(updatedUser.getId())!=null) {
+        if (users.get(updatedUser.getId()) != null) {
             users.replace(updatedUser.getId(), updatedUser);
             log.debug("Фильм обновлен!");
             return updatedUser;
