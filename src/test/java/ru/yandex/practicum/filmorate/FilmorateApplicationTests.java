503c61d59b597d4b10be21c1e43e9b73d01d37ad
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2023-10-02 19:29:21 +0300

    Add files via upload

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,17 +1,6 @@
 package ru.yandex.practicum.filmorate;
 
-
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
-
-import javax.validation.ValidationException;
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class FilmorateApplicationTests {
