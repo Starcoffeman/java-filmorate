4cbd81bd81d167d00ef697036865c35c04fce05b
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2023-10-06 11:20:56 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 
 @RestController
 @Service
+@RequestMapping("/users")
 public class UserService {
 
     InMemoryUserStorage inMemoryUserStorage = new InMemoryUserStorage();
