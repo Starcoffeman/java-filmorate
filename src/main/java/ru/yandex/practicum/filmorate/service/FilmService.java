4e5ea6ad0e506ec23e1dbc2f5d0344bcef43c990
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2023-10-07 19:09:47 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
+/*import ru.yandex.practicum.filmorate.exception.ValidationException;*/
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
@@ -17,7 +17,7 @@ public class FilmService {
     InMemoryFilmStorage inMemoryFilmStorage = new InMemoryFilmStorage();
 
     @PostMapping
-    public Film createFilm(@RequestBody @Valid Film film)  {
+    public Film createFilm(@RequestBody @Valid Film film) {
         inMemoryFilmStorage.addFilm(film);
         return film;
     }
