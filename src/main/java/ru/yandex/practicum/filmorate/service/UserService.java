0cb6bbad68b0a6123f687c1dadd621aba46c7e68
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2023-10-08 21:44:04 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -84,16 +84,13 @@ public class UserService {
     @GetMapping("/{id}/friends/common/{otherId}")
     public ResponseEntity<List<User>> getCommonFriends(@PathVariable int id, int otherId) {
         ArrayList<User> commonFriends = new ArrayList<>();
-        if (inMemoryUserStorage.users.get(id) != null) {
+        if (inMemoryUserStorage.users.get(id) != null || inMemoryUserStorage.users.get(otherId) != null) {
             for (User firstUser : inMemoryUserStorage.users.get(id).getFriendsList()) {
                 for (User secondUser : inMemoryUserStorage.users.get(otherId).getFriendsList()) {
                     if (firstUser == secondUser) {
                         commonFriends.add(inMemoryUserStorage.users.get(id));
                     }
                 }
-            }
-            if (commonFriends.isEmpty()) {
-                return ResponseEntity.ok(commonFriends);
             }
             return ResponseEntity.ok(commonFriends);
         } else {
