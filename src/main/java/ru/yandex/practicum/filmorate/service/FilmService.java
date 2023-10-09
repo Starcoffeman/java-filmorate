09acf0cd91ed501a00c8c2a207e281f47bec5f1f
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2023-10-09 10:59:23 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,13 +1,14 @@
 package ru.yandex.practicum.filmorate.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
-
-
 import javax.validation.Valid;
+import java.util.HashMap;
+
 
 @Service
 @RestController
@@ -17,35 +18,62 @@ public class FilmService {
     InMemoryFilmStorage inMemoryFilmStorage = new InMemoryFilmStorage();
 
     @PostMapping
-    public Film createFilm(@RequestBody @Valid Film film) {
+    public ResponseEntity<Film> createFilm(@RequestBody @Valid Film film) {
         inMemoryFilmStorage.addFilm(film);
-        return film;
+        return ResponseEntity.ok(inMemoryFilmStorage.films.get(film.getId()));
     }
 
     @DeleteMapping("/{id}")
-    public void removeFilm(@PathVariable int id) {
-        inMemoryFilmStorage.removeFilm(id);
+    public ResponseEntity<HashMap<Integer, Film>> removeFilm(@PathVariable Integer id) {
+        if (inMemoryFilmStorage.films.get(id) != null) {
+            inMemoryFilmStorage.removeFilm(id);
+            return ResponseEntity.ok(inMemoryFilmStorage.films);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 
     @GetMapping("/{id}")
-    public Film getFilmById(@PathVariable int id) {
-        return inMemoryFilmStorage.films.get(id);
+    public ResponseEntity<Film> getFilmById(@PathVariable Integer id) {
+        if (inMemoryFilmStorage.films.get(id) != null) {
+            return ResponseEntity.ok(inMemoryFilmStorage.films.get(id));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 
-    @PutMapping("/{id}")
+    @PutMapping
     public void updateFilm(@PathVariable Film updateFilm) throws UserNotFoundException {
         inMemoryFilmStorage.updateFilm(updateFilm);
     }
 
     @PutMapping("/{id}/like/{userId}")
-    public void putLike(@PathVariable int id, int userid) {
-        inMemoryFilmStorage.films.get(id).getLikes().add(userid);
+    public ResponseEntity<Film> putLike(@PathVariable Integer id, @PathVariable Integer userid) {
+        if (inMemoryFilmStorage.films.get(id) != null & inMemoryFilmStorage.films.get(userid) != null) {
+            inMemoryFilmStorage.films.get(id).getLikes().add(userid);
+            return ResponseEntity.ok(inMemoryFilmStorage.films.get(id));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/films/{id}/like/{userId}")
-    public void deleteLike(@PathVariable int id, int userid) {
-        inMemoryFilmStorage.films.get(id).getLikes().remove(userid);
+    public ResponseEntity<Film> deleteLike(@PathVariable Integer id, @PathVariable Integer userid) {
+        if (inMemoryFilmStorage.films.get(id) != null & inMemoryFilmStorage.films.get(userid) != null) {
+            inMemoryFilmStorage.films.get(id).getLikes().remove(userid);
+            inMemoryFilmStorage.films.get(userid).getLikes().remove(id);
+            return ResponseEntity.ok(inMemoryFilmStorage.films.get(id));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
+/*    @GetMapping("/popular?count={count}")
+    public ResponseEntity<List<Film>> getMostPopularFilms(@PathVariable Integer count){
+
+
+    }*/
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
@@ -8,55 +9,83 @@ import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @RestController
 @Service
+@RequestMapping("/users")
 public class UserService {
 
     InMemoryUserStorage inMemoryUserStorage = new InMemoryUserStorage();
 
     @PostMapping
-    public User createFilm(@RequestBody @Valid User user) {
+    public ResponseEntity<User> createFilm(@RequestBody @Valid User user) {
         inMemoryUserStorage.addUser(user);
-        return user;
+        return ResponseEntity.ok(user);
     }
 
     @DeleteMapping("/{id}")
-    public void removeUser(@PathVariable int id) {
-        inMemoryUserStorage.removeUser(id);
+    public ResponseEntity<HashMap<Integer, User>> removeUser(@PathVariable Integer id) {
+        if (inMemoryUserStorage.users.get(id) != null) {
+            inMemoryUserStorage.removeUser(id);
+            return ResponseEntity.ok(inMemoryUserStorage.users);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable int id) {
-        return inMemoryUserStorage.users.get(id);
+    public ResponseEntity<User> getUserById(@PathVariable Integer id) {
+        if (inMemoryUserStorage.users.get(id) != null) {
+            return ResponseEntity.ok(inMemoryUserStorage.users.get(id));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 
-    @PutMapping("/{id}")
+    @PutMapping
     public void updateUser(@PathVariable User updateUser) throws UserNotFoundException {
         inMemoryUserStorage.updateUser(updateUser);
     }
 
     @PutMapping("/{id}/friends/{friendId}")
-    public void addFriend(@PathVariable int id, int friendId) {
-        inMemoryUserStorage.users.get(id).getFriendsList().add(friendId);
+    public ResponseEntity<HashMap<Integer, User>> addFriend(@PathVariable Integer id, @PathVariable Integer friendId) {
+        if (inMemoryUserStorage.users.get(id) != null & inMemoryUserStorage.users.get(friendId) != null) {
+            inMemoryUserStorage.users.get(id).getFriendsList().add(friendId);
+            inMemoryUserStorage.users.get(friendId).getFriendsList().add(id);
+            return ResponseEntity.ok(inMemoryUserStorage.users);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
-    public void removeFriend(@PathVariable int id, int friendId) {
-        inMemoryUserStorage.users.get(id).getFriendsList().remove(friendId);
+    public ResponseEntity<HashMap<Integer, User>> removeFriend(@PathVariable Integer id, @PathVariable Integer friendId) {
+        if (inMemoryUserStorage.users.get(id) != null & inMemoryUserStorage.users.get(friendId) != null) {
+            inMemoryUserStorage.users.get(id).getFriendsList().remove(friendId);
+            inMemoryUserStorage.users.get(friendId).getFriendsList().remove(id);
+            return ResponseEntity.ok(inMemoryUserStorage.users);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
-    public ArrayList<Integer> getCommonFriends(@PathVariable int id, int otherId) {
-        ArrayList<Integer> commonFriends = new ArrayList<>();
-        for (Integer i : inMemoryUserStorage.users.get(id).getFriendsList()) {
-            for (Integer j : inMemoryUserStorage.users.get(otherId).getFriendsList()) {
-                if (i == j) {
-                    commonFriends.add(i);
+    public ResponseEntity<ArrayList<Integer>> getCommonFriends(@PathVariable Integer id, @PathVariable Integer otherId) {
+        if (inMemoryUserStorage.users.get(id) != null & inMemoryUserStorage.users.get(otherId) != null) {
+            ArrayList<Integer> commonFriends = new ArrayList<>();
+            for (Integer i : inMemoryUserStorage.users.get(id).getFriendsList()) {
+                for (Integer j : inMemoryUserStorage.users.get(otherId).getFriendsList()) {
+                    if (i == j) {
+                        commonFriends.add(i);
+                    }
                 }
             }
+            return ResponseEntity.ok(commonFriends);
+        } else {
+            return ResponseEntity.notFound().build();
         }
-        return commonFriends;
     }
 
 }
