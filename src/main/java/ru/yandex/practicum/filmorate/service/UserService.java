35a254c31a4e15cb795de30224ac64bad0032ccd
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2023-10-09 19:21:42 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -56,7 +56,7 @@ public class UserService {
     }
 
     @PutMapping("/{id}/friends/{friendId}")
-    public ResponseEntity<User> addFriend(@PathVariable int id, @PathVariable int friendId) {
+    public ResponseEntity<User> addFriend(@PathVariable int id, @PathVariable Integer friendId) {
         if (inMemoryUserStorage.users.get(id) != null || id >= 0) {
             inMemoryUserStorage.users.get(id).getFriendsList().add(inMemoryUserStorage.users.get(friendId));
             inMemoryUserStorage.users.get(friendId).getFriendsList().add(inMemoryUserStorage.users.get(id));
