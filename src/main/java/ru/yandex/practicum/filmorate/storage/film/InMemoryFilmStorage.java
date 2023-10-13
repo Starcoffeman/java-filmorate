51c83579136be203418e24e80dd9b9cce2df14f9
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2023-10-13 15:22:52 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -57,6 +57,11 @@ public class InMemoryFilmStorage implements FilmStorage {
                 return Integer.compare(film2.getLikes().size(), film1.getLikes().size());
             }
         });
+        
+        if (popularFilms.size() > count) {
+            popularFilms = popularFilms.subList(0, count);
+        }
+
         return popularFilms;
     }
 
