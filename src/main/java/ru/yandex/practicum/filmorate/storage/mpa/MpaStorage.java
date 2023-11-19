cbbb3ccaa7b8f7bc167090ff04d2c2a133d67aa6
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2023-11-20 01:39:33 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/storage/mpa/MpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mpa/MpaStorage.java
@@ -2,11 +2,10 @@ package ru.yandex.practicum.filmorate.storage.mpa;
 
 import ru.yandex.practicum.filmorate.exception.MpaNotFoundException;
 import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.util.List;
-import java.util.Map;
+
 
 public interface MpaStorage {
 
