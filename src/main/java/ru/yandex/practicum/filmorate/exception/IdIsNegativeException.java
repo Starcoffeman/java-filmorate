c56fbc3eaa24c7f573085c84abc4fc972bb625bb
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2024-01-30 10:25:54 +0300

    Delete src directory

--- src/main/java/ru/yandex/practicum/filmorate/exception/IdIsNegativeException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class IdIsNegativeException extends Exception {
-    public IdIsNegativeException(String message) {
-        super(message);
-    }
-}
