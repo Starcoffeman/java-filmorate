edabc940dc5b8291d5dc07390eea8a0febf95825
Valentin <valentin.geras@yandex.ru>
2024-02-02 21:42:22 +0300

    Проверка checkStyle

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -57,8 +57,7 @@ public class FilmController {
     }
 
     @GetMapping("/director/{directorId}")
-    public List<Film> getFilmsOfDirectorSortByLikesOrYears(@PathVariable("directorId") Long id,
-                                                           @RequestParam(defaultValue = "likes") String sortBy) {
+    public List<Film> getFilmsOfDirectorSortByLikesOrYears(@PathVariable("directorId") Long id, @RequestParam(defaultValue = "likes") String sortBy) {
         return filmService.getFilmsOfDirectorSortByLikesOrYears(id, sortBy);
     }
 
