d40e996fda5f32c8202d6d021efc72530ad78211
Starcoffeman <112430941+Starcoffeman@users.noreply.github.com>
2024-02-04 17:40:20 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
 
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
+@Data
 @SuperBuilder
-@NoArgsConstructor
 @AllArgsConstructor
 public class Review {
     private long reviewId;
