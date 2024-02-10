023dccb535132b97b1848fe3ccc7707c58974648
Boltinov <maxim.boltinov@yandex.com>
2024-02-10 22:46:44 +0300

    Feed валидация добавил ограничения на значения полей

--- src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
@@ -2,19 +2,27 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 import ru.yandex.practicum.filmorate.model.enums.EventType;
 import ru.yandex.practicum.filmorate.model.enums.Operation;
 
+import javax.validation.constraints.Positive;
 import java.util.Map;
 
 @Data
 @Builder
 public class Feed {
+    @Positive
     private long eventId;
+    @Positive
     private long timestamp;
+    @Positive
     private long userId;
+    @NonNull
     private EventType eventType;
+    @NonNull
     private Operation operation;
+    @Positive
     private long entityId;
 
     public Map<String, Object> toMapForDB() {
